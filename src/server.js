d7267165d2d539f6a84b6fcf27bffe26356285e6
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-14 12:37:34 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
-const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const app = require("./app");
 const paymentRoutes = require("./routes/paymentRoutes");
-
-app.use(express.json());
-app.use("/api/payments", paymentRoutes);
 
 dotenv.config();
 connectDB();
