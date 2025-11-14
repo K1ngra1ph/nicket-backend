137127cf503db028805a2e72d9df0b4880f7510b
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-14 11:50:04 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 const { initiatePayment, verifyPayment } = require("../controllers/paymentController");
 
 const router = express.Router();
