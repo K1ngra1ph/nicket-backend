fc2ae15ab09b883c8773c4d6df22b5e75bbfd8d0
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-15 08:56:46 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
+
 const userRoutes = require("./routes/userRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const merchantRoutes = require("./routes/merchantRoutes");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({ origin: [
   "https://nicket-lilac.vercel.app", 
   "https://nicket-lilac.vercel.app/game"
 ], credentials: true }));
+
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("🔥 Nicket Backend running with MongoDB!"));
 
