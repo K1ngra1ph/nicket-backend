2ec3ee353aec839532a63d7c517b5b76d041ab0c
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-17 22:34:26 +0100

    Update Payment.js

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const paymentSchema = new mongoose.Schema(
   {
     paymentReference: { type: String, unique: true, required: true },
-    amount: Number,
+    transactionReference: { type: String, index: true },
+    amount: { type: Number, required: true },
+    amountPaid: { type: Number, default: 0 },
     eventValue: String,
 
     name: String,
     email: String,
     phone: String,
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 
     status: { type: String, default: "pending" },
-    transactionReference: String
+    metaData: { type: Object },
   },
   { timestamps: true }
 );
