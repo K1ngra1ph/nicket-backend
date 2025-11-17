f2b90d6c8d7221bfd669d84eff1e8dad99c45bce
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-17 18:02:05 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { initiatePayment, verifyPayment } = require("../controllers/paymentController");
+const {
+  initiatePayment,
+  verifyPayment,
+  monnifyWebhook
+} = require("../controllers/paymentController");
 
 const router = express.Router();
+
 router.post("/initiate-payment", initiatePayment);
 router.post("/verify-payment", verifyPayment);
+router.post("/webhook", express.raw({ type: "*/*" }), monnifyWebhook);
 
 module.exports = router;
