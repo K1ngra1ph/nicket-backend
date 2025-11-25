993f545663ea4fb7a14c187b2f957021630cbe07
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-25 17:34:25 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { initiatePayment, monnifyWebhook } = require("../controllers/paymentController");
+const { initiatePayment, verifyPayment, monnifyWebhook } = require("../controllers/paymentController"):
 
-// POST /api/payments/initiate-payment
 router.post("/initiate-payment", initiatePayment);
 
-// POST /api/payments/monnify-webhook
-// Webhook must use raw body parser for signature validation
+router.get("/verify-payment", verifyPayment);
+
 router.post("/monnify-webhook", express.raw({ type: "*/*" }), monnifyWebhook);
 
 module.exports = router;
