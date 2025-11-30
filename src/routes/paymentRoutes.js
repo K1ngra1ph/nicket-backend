ebefa01ee75da0f2f3796546dfad2d6a2203d25b
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-11-30 15:05:19 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const initiatePayment = require("../controllers/payment/initiatePayment");
 const verifyPayment = require("../controllers/payment/verifyPayment");
 const redirectAfterPayment = require("../controllers/payment/redirectAfterPayment");
+const getPaymentReference = require("../controllers/payment/getPaymentReference");
 const monnifyWebhook = require("../controllers/payment/monnifyWebhook");
 
 router.post("/initiate-payment", initiatePayment);
 router.get("/verify/:paymentReference", verifyPayment);
 router.get("/redirect", redirectAfterPayment);
+router.get("/get-payment-reference", getPaymentReference);
 
 router.post("/monnify-webhook",
   express.raw({ type: "*/*" }),
