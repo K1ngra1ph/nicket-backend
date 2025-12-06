3926cd58c6c8911d3e1a5322e8da5a65a9079630
Rapahel Agboli <164863742+K1ngra1ph@users.noreply.github.com>
2025-12-06 04:03:04 +0000

    Initial commit from Codespace

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import merchantRoutes from "./routes/merchantRoutes.js";
 import numberRoutes from "./routes/numbers.js";
 import eventRoutes from "./routes/eventRoutes.js";
+import adminExportRoutes from "./routes/adminExport.js";
 
 const app = express();
 
 app.use(express.json());
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "*",
@@ -33,6 +35,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/numbers", numberRoutes);
 app.use("/api/merchant", merchantRoutes);
 app.use("/events", eventRoutes);
+app.use("/api/admin", adminExportRoutes);
 
 app.get("/", (req, res) => {
   res.send("🔥 Nicket Backend running with MongoDB!");
